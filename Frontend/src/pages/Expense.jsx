3d26d14b76e3ea9fb25efe7a9a6360b6c114cb36
NareshAdhe <nareshadhe01@gmail.com>
2025-01-03 07:47:39 +0530

    Updated

--- Frontend/src/pages/Expense.jsx
+++ Frontend/src/pages/Expense.jsx
@@ -46,7 +46,7 @@ const Expense = () => {
     visible: {
       opacity: 1,
       x: 0,
-      transition: { duration: 0.2, ease: "easeInOut", delay: 0.5 },
+      transition: { duration: 0.2, ease: "easeInOut", delay: 0.4 },
     },
     hover: {
       scale: 1.05,
