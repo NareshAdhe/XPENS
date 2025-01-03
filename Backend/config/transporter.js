8b721705bfae2001d779d95eda49e405153c8cce
NareshAdhe <nareshadhe01@gmail.com>
2025-01-04 04:49:19 +0530

    Updated

--- Backend/config/transporter.js
+++ Backend/config/transporter.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
+  host: "smtp.gmail.com",
   port: 587,
   auth: {
     user: process.env.SMTP_USER,
